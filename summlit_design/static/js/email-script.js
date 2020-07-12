2dc44d7b9c7b2a1913120eea4d32437111ab74ed
fe-iron <elahifaiz00@gmail.com>
2020-07-12 21:08:57 +0530

    little change

--- summlit_design/static/js/email-script.js
+++ summlit_design/static/js/email-script.js
@@ -4,5 +4,5 @@ const form = document.forms['Emails for summlit']
 
 form.addEventListener('submit', e => {
 e.preventDefault()
-fetch(scriptURL, { method: 'POST', body: new FormData(form)}).then(response => alert("Thanks for Contacting us..! We Will Contact You Soon...")).catch(error => console.error('Error!', error.message))
+fetch(scriptURL, { method: 'POST', mode:'no-cors', body: new FormData(form)}).then(response => alert("Thanks for Contacting us..! We Will Contact You Soon...")).catch(error => console.error('Error!', error.message))
 })
